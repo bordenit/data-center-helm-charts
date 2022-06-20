7ba6029691d5e790631decd3d69a4bb60e8d7d98
jborden <jborden@blackwidow.lab.bordencastle.com>
2022-06-20 08:24:23 -0400

    synchrony path fix

--- src/test/java/test/IngressTest.java
+++ src/test/java/test/IngressTest.java
@@ -142,7 +142,7 @@ class IngressTest {
     }
 
     @ParameterizedTest
-    @EnumSource(value = Product.class, names = {"jira", "confluence", "bamboo", "crowd"})
+    @EnumSource(value = Product.class, names = {"jira", "confluence", "bamboo"})
     void jira_ingress_host_port(Product product) throws Exception {
         final var resources = helm.captureKubeResourcesFromHelmChart(product, Map.of(
                 "ingress.host", "myhost.mydomain",
@@ -154,7 +154,7 @@ class IngressTest {
     }
 
     @ParameterizedTest
-    @EnumSource(value = Product.class, names = {"jira", "confluence", "bamboo", "crowd"})
+    @EnumSource(value = Product.class, names = {"jira", "confluence", "bamboo"})
     void jira_ingress_port(Product product) throws Exception {
         final var resources = helm.captureKubeResourcesFromHelmChart(product, Map.of(
                 "ingress.host", "myhost.mydomain"));
@@ -165,7 +165,7 @@ class IngressTest {
     }
 
     @ParameterizedTest
-    @EnumSource(value = Product.class, names = {"jira", "confluence", "bamboo", "crowd"})
+    @EnumSource(value = Product.class, names = {"jira", "confluence", "bamboo"})
     void jira_ingress_port_http(Product product) throws Exception {
         final var resources = helm.captureKubeResourcesFromHelmChart(product, Map.of(
                 "ingress.host", "myhost.mydomain",
@@ -380,7 +380,7 @@ class IngressTest {
 
         org.assertj.core.api.Assertions.assertThat(ingressPaths).containsExactlyInAnyOrder(
                 "/confluence-tmp",
-                "/confluence-tmp/synchrony",
+                "/synchrony",
                 "/confluence-tmp/setup",
                 "/confluence-tmp/bootstrap");
     }
@@ -420,7 +420,7 @@ class IngressTest {
 
         org.assertj.core.api.Assertions.assertThat(ingressPaths).containsExactlyInAnyOrder(
                 "/ingress",
-                "/ingress/synchrony",
+                "/synchrony",
                 "/ingress/setup",
                 "/ingress/bootstrap");
     }
@@ -460,7 +460,7 @@ class IngressTest {
 
         org.assertj.core.api.Assertions.assertThat(ingressPaths).containsExactlyInAnyOrder(
                 "/ingress",
-                "/ingress/synchrony",
+                "/synchrony",
                 "/ingress/setup",
                 "/ingress/bootstrap");
     }
